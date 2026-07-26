510540c811b8a5e02d216e869a5b09e1bf713a97
Ritika <sb8813128@gmail.com>
2026-07-26 23:47:57 +0530

    Move addtocart into cart.js

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addtocart(productId){
+        let matchingItem;
+
+      cart.forEach((Cartitem) =>{
+        if(productId === Cartitem.productId){
+           matchingItem = Cartitem;
+        }
+      });
+      if(matchingItem) {
+        matchingItem.quantity++;
+      }
+      else{
+cart.push({
+        productId: productId,
+        quantity:1
+        });
+      }
+}
--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,4 +1,4 @@
-import{cart} from '../data/cart.js';
+import{cart, addtocart} from '../data/cart.js';
 import { products } from '../data/products.js';
 let productshtml = '';
 
@@ -58,34 +58,23 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid')
 .innerHTML = productshtml;
 
+
+function updateCartquantity(){
+        let cartQuantity = 0;
+            cart.forEach((Cartitem)=>{
+                cartQuantity+= Cartitem.quantity;
+            });
+
+            document.querySelector('.js-cart-quantity')
+            .innerHTML = cartQuantity;
+}
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) => {
    button.addEventListener('click' ,() =>{
       const productId = button.dataset.productId;
-      
-      let matchingItem;
+      addtocart(productId);
+      updateCartquantity();
 
-      cart.forEach((item) =>{
-        if(productId === item.productId){
-           matchingItem = item;
-        }
-      });
-      if(matchingItem) {
-        matchingItem.quantity++;
-      }
-      else{
-cart.push({
-        productId: productId,
-        quantity:1
-        });
-      }
-      let cartQuantity = 0;
-      cart.forEach((item)=>{
-        cartQuantity+= item.quantity;
-      });
-
-      document.querySelector('.js-cart-quantity')
-      .innerHTML = cartQuantity;
     //   console.log(cartQuantity);
     //   console.log(cart);
       
