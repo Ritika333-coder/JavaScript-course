3c16c7fdcfb8528e6f619136d29efb7a2a2b649e
Ritika <sb8813128@gmail.com>
2026-08-02 19:44:39 +0530

    Make payment summary interactive

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -5,10 +5,10 @@ import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 // import dayjs from 'https://unpkg.com/dayjs@1.11.10/dayjs.min.js';
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-hello();
-const today = dayjs();
-const deliveryDate = today.add(7 ,'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from './paymentSummary.js';
+
+
+
 
 export function renderOrderSummary(){
 
@@ -132,6 +132,8 @@ export function renderOrderSummary(){
         `.js-cart-item-container-${productId}`
     );
     container.remove();
+
+   renderPaymentSummary();
     });
     });
 
@@ -141,6 +143,8 @@ export function renderOrderSummary(){
             const {productId,deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
+
+            renderPaymentSummary();
         });
     });
 }
